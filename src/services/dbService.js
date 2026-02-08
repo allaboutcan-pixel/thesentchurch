9c48734fd02306c3c9d525625f44def482f0742c
allaboutcan-pixel <allaboutcan@gmail.com>
2026-02-08 10:17:37 -0800

    Fix missing Quote icon ReferenceError

--- src/services/dbService.js
+++ src/services/dbService.js
@@ -264,12 +264,6 @@ export const dbService = {
     deleteNotice: (id) => dbService.deleteItem(NOTICES, id),
     deleteGalleryItem: (id) => dbService.deleteItem(DB_COLLECTIONS.GALLERY, id),
     deleteColumn: (id) => dbService.deleteItem(COLUMNS, id),
-    deleteDailyWord: (id) => dbService.deleteItem(DAILY_WORD, id),
-
-    // Specific Fetchers for Daily Word
-    getDailyWords: (count = 50) => dbService.fetchItems(DAILY_WORD, count),
-    addDailyWord: (data) => dbService.addItem(DAILY_WORD, data),
-    updateDailyWord: (id, data) => dbService.updateItem(DAILY_WORD, id, data),
 
     // Calendar
     getCalendarEvents: async () => {
